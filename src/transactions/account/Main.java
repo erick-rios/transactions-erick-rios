8c1c48866d8165c820ab117d1eb3b78d50c96399
erick-rios <erick_rios_gonzalez@ciencias.unam.mx>
2022-06-09 22:47:13 -0500

    1.0.1

---
+++ src/transactions/account/Main.java
@@ -0,0 +1,17 @@
+package transactions.account;
+
+import java.util.Arrays;
+
+public class Main{
+    public static void main(String[] args) {
+        Account accountFrom = new Account(1);
+		Account accountTo = new Account(2);
+		
+		accountFrom.sendMoneyToAccount(accountTo, 100);
+		accountFrom.withdrawMoney(1000);
+		
+		System.out.println(Arrays.toString(accountFrom.getTransactions()));
+		System.out.println(Arrays.toString(accountTo.getTransactions()));
+		
+    }
+}
--- src/transactions/operations/StandardAccountOperations.java
+++ src/transactions/operations/StandardAccountOperations.java
@@ -1,5 +1,5 @@
 package transactions.operations;
 
-public enum StandardAccountOperations[
+public enum StandardAccountOperations{
     MONEY_TRANSFER_SEND, MONEY_TRANSFER_RECEIVE, WITHDRAW;
-]
+}
