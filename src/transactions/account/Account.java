4926e14186adf7596fd980236a0c5aed571e90ab
erick-rios <erick_rios_gonzalez@ciencias.unam.mx>
2022-06-09 23:08:24 -0500

    final version

--- src/transactions/account/Account.java
+++ src/transactions/account/Account.java
@@ -7,11 +7,12 @@ import java.util.Objects;
 
 public class Account {
 
-	private static final int DEFAULT_TRANSACTIONS_AMOUNT = 10;
+    private static final int DEFAULT_TRANSACTIONS_AMOUNT = 10;
 
 	private int id;
 	private Transaction[] transactions;
 	private int lastTransactionIndex;
+
 
 	{
 		transactions = new Transaction[DEFAULT_TRANSACTIONS_AMOUNT];
@@ -22,14 +23,9 @@ public class Account {
 	}
 
 	public void sendMoneyToAccount(Account accountTo, double moneyAmount) {
-		if (accountTo == null) {
-			return;
-		}
-		if (moneyAmount <= 0) {
-			return;
-		}
-		Transaction transaction = new Transaction(this, accountTo, moneyAmount,
-				StandardAccountOperations.MONEY_TRANSFER_SEND);
+		if (accountTo == null) return;
+		if (moneyAmount <= 0) return;
+		Transaction transaction = new Transaction(this, accountTo, moneyAmount, StandardAccountOperations.MONEY_TRANSFER_SEND);
 		addTransaction(transaction);
 		accountTo.receiveMoney(this, moneyAmount);
 	}
@@ -41,8 +37,7 @@ public class Account {
 		if (moneyAmount <= 0) {
 			return;
 		}
-		Transaction transaction = new Transaction(accountFrom, this, moneyAmount,
-				StandardAccountOperations.MONEY_TRANSFER_RECEIVE);
+		Transaction transaction = new Transaction(accountFrom, this, moneyAmount, StandardAccountOperations.MONEY_TRANSFER_RECEIVE);
 		addTransaction(transaction);
 	}
 
@@ -54,11 +49,11 @@ public class Account {
 			transactions = Arrays.copyOf(transactions, transactions.length << 1);
 		}
 		transactions[lastTransactionIndex++] = transaction;
+        
 	}
 
 	public void withdrawMoney(double moneyAmount) {
-		Transaction transaction = new Transaction(this, null, moneyAmount,
-				StandardAccountOperations.WITHDRAW);
+		Transaction transaction = new Transaction(this, null, moneyAmount, StandardAccountOperations.WITHDRAW);
 		addTransaction(transaction);
 	}
 
@@ -86,12 +81,6 @@ public class Account {
 
 	}
 
-	// ===== STREAM API Solution
-//	public Transaction[] getTransactions() {
-//		return Arrays.stream(transactions)
-//				.filter(Objects::nonNull)
-//				.toArray(Transaction[]::new);
-//	}
 
 	public static class Transaction {
 
@@ -100,8 +89,7 @@ public class Account {
 		private double moneyAmount;
 		private StandardAccountOperations operation;
 
-		private Transaction(Account accountFrom, Account accountTo, double moneyAmount,
-				StandardAccountOperations moneyTransferOperation) {
+		private Transaction(Account accountFrom, Account accountTo, double moneyAmount, StandardAccountOperations moneyTransferOperation) {
 			this.accountFrom = accountFrom;
 			this.accountTo = accountTo;
 			this.moneyAmount = moneyAmount;
@@ -142,8 +130,7 @@ public class Account {
 
 		@Override
 		public String toString() {
-			return "Transaction [accountFrom=" + accountFrom + ", accountTo=" + accountTo
-					+ ", moneyAmount=" + moneyAmount + ", operation=" + operation + "]";
+			return "Transaction [accountFrom=" + accountFrom + ", accountTo=" + accountTo + ", moneyAmount=" + moneyAmount + ", operation=" + operation + "]";
 		}
 
 	}
